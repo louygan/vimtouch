add8822eb0ef11a56aa40d60d8b3097b94be8be8
Louie GAN <louie.gan@ericsson.com>
2018-05-30 16:02:52 +0800

    update .gitmodules prepare-clean-checkout.sh to solve download error

--- prepare-clean-checkout.sh
+++ prepare-clean-checkout.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 cd jni
-rm -r vim
-rm -r libiconv
+rm -rf vim
+rm -rf libiconv
+rm -rf libncruse
 wget ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
-tar jxvf vim-7.4.tar.bz2
+tar jxvf vim-7.3.tar.bz2
 mv vim73 vim
 rm vim/src/auto/config.h
 rm vim/src/feature.h
@@ -15,6 +16,6 @@ wget http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.14.tar.gz
 tar xzvf libiconv-1.14.tar.gz
 mv libiconv-1.14 libiconv
 rm libiconv-1.14.tar.gz
-git clone git@github.com:momodalo/android_external_libncurses.git -b vimtouch libncurses
+git clone https://github.com/momodalo/android_external_libncurses.git -b vimtouch libncruse
 cd ..
 
